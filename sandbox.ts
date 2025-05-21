6578ef854f59ea84674a336461a4cec061b04c4d
ishaan-mah <ishaanmaheshwari2001@gmail.com>
2025-05-20 22:55:01 -0400

    lesson 4 update

--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,25 @@
-let character = "mario";
+let names =['luigi','mario','yoshi'];
+names.push('toad');
 
-const circle = (diameter : number) => {
-    return Math.PI * diameter;
+let numbers = [10, 20, 30, 40];
+numbers.push(25);
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
 }
-
-
-console.log(circle(10));
