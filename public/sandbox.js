f9610e3a1c3973fee7e7a5cec7e72a0baad89b1a
ishaan-mah <ishaanmaheshwari2001@gmail.com>
2025-05-21 00:19:15 -0400

    Lesson 8 updated

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hello from sandbox.ts");
+let greet;
+greet = () => {
+    console.log("Hello, world!");
+};
+const add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, "20");
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,17 @@
-console.log("Hello from sandbox.ts");
+let greet: Function;
+greet =() => {
+    console.log("Hello, world!");
+}
+const add = (a:number, b:number, c?:number|string) => {
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 10, "20");
+
+const minus = (a:number, b:number):number => {
+    return a - b;
+}
+
+let result = minus(10, 7);
+console.log(result);
